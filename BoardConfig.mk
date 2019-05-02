bdf45f159ba8db307abd463a12718b2728413d4c
Mattijs Korpershoek <mkorpershoek@baylibre.com>
2019-05-01 18:08:56 -0700

    BoardConfig: define TARGET_CPU_* variables
    Signed-off-by: Mattijs Korpershoek <mkorpershoek@baylibre.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -17,7 +17,19 @@
 # Use the non-open-source parts, if they're present
 -include vendor/mediatek/mt8183/BoardConfigVendor.mk
 
-TARGET_ARCH := arm
-TARGET_ARCH_VARIANT := armv7-a-neon
-TARGET_CPU_ABI := armeabi-v7a
-TARGET_CPU_ABI2 := armeabi
+# Primary Arch
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := cortex-a73
+
+# Secondary Arch
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv8-a
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a53
+
+TARGET_IS_64_BIT := true
+TARGET_USES_64_BIT_BINDER := true

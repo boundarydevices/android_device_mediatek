461800fcc397b806b12a904f81b0babfdc9946e3
Mattijs Korpershoek <mkorpershoek@baylibre.com>
2019-06-18 21:27:58 +0000

    binaries: first drop for boot to ui
    sha1sums:
    baef6e39e7fec64b73db937635f74d16e9047e8a  preloader_aiv8183m1_64_bsp.bin
    c7504ddf626b2541f49700b6e16486419ddd3ef8  lk.img
    1a9bdb0e7d492b891a02d00d99f0627858835143  tee.img
    1157453ccd3e772151681fa0fb17a6ec54a4ab94  logo.bin
    6c277f31afba03b9bfd38a827228831eee801f8c  sspm.img
    
    Content: custom preloader and lk build by BayLibre with:
    - disabled SELinux
    - disabled watchdog as watchdog driver does not work
    
    Tee:
    - comes from aiv8183m1_64_bsp_alps-mp-p0.mp6-V1.92_userdebug release
    
    Userdata:
    - empty ext4 image, to ensure that SPFlashtool formats
      the partition properly
    
    Scatterfile:
    - input for SPFlashtool to be able to fully reflash the device
    
    Signed-off-by: Mattijs Korpershoek <mkorpershoek@baylibre.com>

--- device.mk
+++ device.mk
@@ -99,3 +99,13 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.wifi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.xml \
+
+# Flashing binaries
+PRODUCT_COPY_FILES += \
+    device/mediatek/mt8183/binaries/images/preloader_aiv8183m1_64_bsp.bin:$(TARGET_OUT)/preloader_aiv8183m1_64_bsp.bin \
+    device/mediatek/mt8183/binaries/images/lk.img:$(TARGET_OUT)/lk.img \
+    device/mediatek/mt8183/binaries/images/tee.img:$(TARGET_OUT)/tee.img \
+    device/mediatek/mt8183/binaries/images/sspm.img:$(TARGET_OUT)/sspm.img \
+    device/mediatek/mt8183/binaries/images/logo.bin:$(TARGET_OUT)/logo.bin \
+    device/mediatek/mt8183/binaries/images/userdata.img:$(TARGET_OUT)/userdata.img \
+    device/mediatek/mt8183/binaries/images/MT8183_full_scatter.txt:$(TARGET_OUT)/MT8183_full_scatter.txt \

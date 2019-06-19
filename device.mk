cfb9fdc3b6a7bfc093e919495f94c016b4e98d5d
Alexandre Bailon <abailon@baylibre.com>
2019-06-19 17:28:33 +0000

    gatekeeper: add mtk gatekeeper@1.0 hal
    This enables the stub mediatek implementation found here:
      device/mediatek/common/gatekeeper
    
    Signed-off-by: Alexandre Bailon <abailon@baylibre.com>
    Signed-off-by: Mattijs Korpershoek <mkorpershoek@baylibre.com>

--- device.mk
+++ device.mk
@@ -40,3 +40,10 @@ PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl \
     android.hardware.keymaster@3.0-service
 
+PRODUCT_PACKAGES += \
+    gatekeeper.mtk \
+    android.hardware.gatekeeper@1.0-impl \
+    android.hardware.gatekeeper@1.0-service
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.hardware.gatekeeper=mtk
+

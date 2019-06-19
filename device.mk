d6f8ea0be697c1c8dee3aec99eb9f7e970673d82
Mattijs Korpershoek <mkorpershoek@baylibre.com>
2019-06-19 23:43:09 +0000

    build: enable treble via PRODUCT_SHIPPING_API_LEVEL
    Treble is enabled by default if PRODUCT_SHIPPING_API_LEVEL >= 26
    
    Notes:
    - When PRODUCT_SHIPPING_API_LEVEL >= 27, TARGET_USES_MKE2FS must be true
    
    Test: boot to UI; getprop ro.treble.enabled returns true
    
    Signed-off-by: Mattijs Korpershoek <mkorpershoek@baylibre.com>

--- device.mk
+++ device.mk
@@ -20,6 +20,9 @@ else
 LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
 endif
 
+# Shipping API level to Android P (28)
+PRODUCT_SHIPPING_API_LEVEL := 28
+
 PRODUCT_COPY_FILES := \
 	$(LOCAL_KERNEL):kernel
 

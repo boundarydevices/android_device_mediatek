63bdd7e8caeb3f6497b0af29c47a103aad010028
Alexandre Bailon <abailon@baylibre.com>
2019-06-19 17:28:03 +0000

    keymaster: add default keymaster@3.0 hal
    This enables the default AOSP implementation found here:
      hardware/interfaces/keymaster/3.0/default
    
    Signed-off-by: Alexandre Bailon <abailon@baylibre.com>
    Signed-off-by: Mattijs Korpershoek <mkorpershoek@baylibre.com>

--- device.mk
+++ device.mk
@@ -34,3 +34,9 @@ $(call inherit-product-if-exists, vendor/mediatek/mt8183/device-vendor.mk)
 PRODUCT_PACKAGES += \
     android.hardware.health@2.0-service \
     android.hardware.health@2.0-service.default
+
+# Security
+PRODUCT_PACKAGES += \
+    android.hardware.keymaster@3.0-impl \
+    android.hardware.keymaster@3.0-service
+

052d4607d44f209664005eb4dea5e4e14dfa05bf
Mattijs Korpershoek <mkorpershoek@baylibre.com>
2019-06-26 22:58:19 +0000

    health: use batteryless implementation
    mt8167 has only an AC charger, so use batteryless health implementation
    from device/mediatek/common
    
    Note: to be able to run the service during init, we also need the
    appropriate SEPolicy rules to be imported.
    
    See:
    - https://source.android.com/devices/tech/power/batteryless
    - https://source.android.com/devices/tech/health/implementation
    
    Signed-off-by: Mattijs Korpershoek <mkorpershoek@baylibre.com>

--- device.mk
+++ device.mk
@@ -35,7 +35,7 @@ PRODUCT_COPY_FILES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.0-service \
+    android.hardware.health@2.0-service.batteryless \
 
 # Security
 PRODUCT_PACKAGES += \

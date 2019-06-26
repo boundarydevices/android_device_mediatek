4347bcf8bfa9cc53d034c8cb5eb946d30e2b4999
Mattijs Korpershoek <mkorpershoek@baylibre.com>
2019-06-26 22:58:19 +0000

    health: use batteryless implementation
    mt8183 has only an AC charger, so use batteryless health implementation
    from device/mediatek/common
    
    Note: to be able to run the service during init, we also need the
    appropriate SEPolicy rules to be imported.
    
    See:
    - https://source.android.com/devices/tech/power/batteryless
    - https://source.android.com/devices/tech/health/implementation
    
    Signed-off-by: Mattijs Korpershoek <mkorpershoek@baylibre.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -64,7 +64,8 @@ BOARD_CACHEIMAGE_PARTITION_SIZE    := 16777216
 BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
 
 BOARD_SEPOLICY_DIRS += \
-        device/mediatek/mt8183/sepolicy
+        device/mediatek/mt8183/sepolicy/ \
+        device/mediatek/common/sepolicy/vendor \
 
 DEVICE_MANIFEST_FILE := device/mediatek/mt8183/manifest.xml
 

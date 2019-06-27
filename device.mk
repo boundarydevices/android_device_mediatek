faf7756df747343ac0a43ad1a4b3243672749d9f
Mattijs Korpershoek <mkorpershoek@baylibre.com>
2019-06-27 16:39:25 +0000

    device: remove unknown health package
    A search for "android.hardware.health@2.0-service.default" in all
    android makefiles (.mk, bp) returns nothing relevant.
    
    Remove this unknown package.
    
    Signed-off-by: Mattijs Korpershoek <mkorpershoek@baylibre.com>

--- device.mk
+++ device.mk
@@ -36,7 +36,6 @@ PRODUCT_COPY_FILES += \
 # Health
 PRODUCT_PACKAGES += \
     android.hardware.health@2.0-service \
-    android.hardware.health@2.0-service.default
 
 # Security
 PRODUCT_PACKAGES += \

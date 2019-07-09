7b4c48ee25ecba816bd153442c22aab1e1473e95
Mattijs Korpershoek <mkorpershoek@baylibre.com>
2019-07-09 20:35:47 +0000

    audio: enable mediatek audio hal
    This patch enables the mediatek audio hal from:
        device/mediatek/common/audio/
    
    Test:
        <Plug headphones>
        Boot to UI,
        Tap on the screen, hear systemUI sounds
        adb push 'music_1.mp3' /sdcard/Music/ && \
        adb push 'music_2.mp3' /sdcard/Music/ && \
        adb reboot
        start com.android.music player and play some music
    
    Signed-off-by: Mattijs Korpershoek <mkorpershoek@baylibre.com>

--- device.mk
+++ device.mk
@@ -75,16 +75,15 @@ PRODUCT_PACKAGES += \
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@2.0-service \
-    android.hardware.audio@4.0-impl \
+    android.hardware.audio@4.0-service.mediatek \
     android.hardware.audio.effect@4.0-impl \
-    audio.stub.default \
 
 PRODUCT_COPY_FILES += \
     device/mediatek/mt8183/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
     device/mediatek/mt8183/audio/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
     device/mediatek/mt8183/audio/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
     device/mediatek/mt8183/audio/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
+    device/mediatek/mt8183/audio/audio_hal_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_hal_configuration.xml \
 
 # DRM (Digital Rights Management)
 PRODUCT_PACKAGES += \

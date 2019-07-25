1dc80fabd5ad7b47cb00d4c28219ab9258204ce7
Mattijs Korpershoek <mkorpershoek@baylibre.com>
2019-07-25 22:02:14 +0000

    binaries: first drop for flashing / boot to console
    Content:
    - MBR_EMMC: master boot record
    - bl2.img: baylibre bootloader
    - dl_addr.ini: used for DA
    - fip.bin: baylibre bootloaders (firmware package image)
    - lk.bin: used as mtk download agent (DA)
    
    - fastboot-{darwin,linux-x86_64}: prebuild fastboot tool required for
      flashing scripts
    - fbtool.py: low level flashing per serial link
    - flashimage.py: the main flash script to be called from commandline
    - flashproc.py: description of the flashing steps (per product)
    
    Signed-off-by: Mattijs Korpershoek <mkorpershoek@baylibre.com>

--- device.mk
+++ device.mk
@@ -99,3 +99,16 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.wifi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.xml \
+
+# Flashing tool + prebuilt binaries
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/flashimage/flashimage.py:flashimage.py \
+    $(LOCAL_PATH)/flashimage/flashproc.py:flashproc.py \
+    $(LOCAL_PATH)/flashimage/fbtool.py:fbtool.py \
+    $(LOCAL_PATH)/flashimage/fastboot-darwin:fastboot-darwin \
+    $(LOCAL_PATH)/flashimage/fastboot-linux-x86_64:fastboot-linux-x86_64 \
+    $(LOCAL_PATH)/flashimage/dl_addr.ini:dl_addr.ini \
+    $(LOCAL_PATH)/flashimage/lk.bin:lk.bin \
+    $(LOCAL_PATH)/flashimage/bl2.img:bl2.img \
+    $(LOCAL_PATH)/flashimage/MBR_EMMC:MBR_EMMC \
+    $(LOCAL_PATH)/flashimage/fip.bin:fip.bin

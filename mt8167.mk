96d0b1fd4d185ff06110f0dc9763aff3e16c12f4
Alexandre Bailon <abailon@baylibre.com>
2019-07-25 21:23:55 +0000

    mt8167.mk: inherit-product for 64bits and aosp
    TARGET_ARCH := arm64 so we *must* inherit from core_64_bit.mk
    
    aosp_base.mk is the exact same content as full_base.mk but must be
    included *after* core_64_bit.mk
    
    Signed-off-by: Alexandre Bailon <abailon@baylibre.com>
    Signed-off-by: Mattijs Korpershoek <mkorpershoek@baylibre.com>

--- mt8167.mk
+++ mt8167.mk
@@ -13,7 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 $(call inherit-product, device/mediatek/mt8167/device.mk)
 
 PRODUCT_NAME := mt8167

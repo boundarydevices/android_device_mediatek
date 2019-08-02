e4d0f50f641d1444ee8d0d5bd507f71d6cfb9d19
Alexandre Bailon <abailon@baylibre.com>
2019-08-02 15:19:52 +0000

    kernel: update LOCAL_KERNEL := Image
    With U-Boot, we currently do not support:
    - compressed kernels
    - appended dtb in kernel binaries
    
    Since we want to switch to U-Boot, we must generate a different kernel
    binary with the above features removed.
    
    Update LOCAL_KERNEL to Image.
    
    Signed-off-by: Alexandre Bailon <abailon@baylibre.com>
    Signed-off-by: Mattijs Korpershoek <mkorpershoek@baylibre.com>

--- device.mk
+++ device.mk
@@ -15,7 +15,7 @@
 #
 
 ifeq ($(TARGET_PREBUILT_KERNEL),)
-LOCAL_KERNEL := device/mediatek/mt8183-kernel/Image.gz-dtb
+LOCAL_KERNEL := device/mediatek/mt8183-kernel/Image
 else
 LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
 endif

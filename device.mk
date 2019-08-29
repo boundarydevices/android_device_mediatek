10c8cd229a0b7866e8f1489aced37fa27d729356
Mattijs Korpershoek <mkorpershoek@baylibre.com>
2019-08-29 17:15:00 +0000

    permissions: add software.appwidget permission file
    During boot, the system crashes with the following error:
    
    E SystemServiceRegistry: No service published for: appwidget
    E SystemServiceRegistry: android.os.ServiceManager$ServiceNotFoundException: No service published for: appwidget
    E SystemServiceRegistry:         at android.os.ServiceManager.getServiceOrThrow(ServiceManager.java:148)
    E SystemServiceRegistry:         at android.app.SystemServiceRegistry$81.createService(SystemServiceRegistry.java:911)
    E SystemServiceRegistry:         at android.app.SystemServiceRegistry$81.createService(SystemServiceRegistry.java:908)
    E SystemServiceRegistry:         at android.app.SystemServiceRegistry$CachedServiceFetcher.getService(SystemServiceRegistry.java:1173)
    E SystemServiceRegistry:         at android.app.SystemServiceRegistry.getSystemService(SystemServiceRegistry.java:1089)
    E SystemServiceRegistry:         at android.app.ContextImpl.getSystemService(ContextImpl.java:1741)
    E SystemServiceRegistry:         at android.content.Context.getSystemService(Context.java:3277)
    E SystemServiceRegistry:         at com.android.server.usage.AppStandbyController.onBootPhase(AppStandbyController.java:364)
    E SystemServiceRegistry:         at com.android.server.usage.UsageStatsService.onBootPhase(UsageStatsService.java:210)
    E SystemServiceRegistry:         at com.android.server.SystemServiceManager.startBootPhase(SystemServiceManager.java:155)
    E SystemServiceRegistry:         at com.android.server.SystemServer.startOtherServices(SystemServer.java:1713)
    E SystemServiceRegistry:         at com.android.server.SystemServer.run(SystemServer.java:444)
    E SystemServiceRegistry:         at com.android.server.SystemServer.main(SystemServer.java:303)
    E SystemServiceRegistry:         at java.lang.reflect.Method.invoke(Native Method)
    E SystemServiceRegistry:         at com.android.internal.os.RuntimeInit$MethodAndArgsCaller.run(RuntimeInit.java:492)
    E SystemServiceRegistry:         at com.android.internal.os.ZygoteInit.main(ZygoteInit.java:891)
    
    appwidget is also used by Launcher3, the default AOSP launcher.
    
    Add the appropriate permission file to fix this issue.
    
    Test: AOSP Launcher3 can start on boot.
    
    Signed-off-by: Mattijs Korpershoek <mkorpershoek@baylibre.com>

--- device.mk
+++ device.mk
@@ -119,6 +119,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
 
+# app widget, needed for default launcher3
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.app_widgets.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.app_widgets.xml \
+
 # DEVICE_PACKAGE_OVERLAYS for the device should be before
 # including common overlays since the one listed first
 # takes precedence.

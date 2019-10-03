7491c647a38e0ddd9678aed05f6e0d2b3cb7bc91
Alexandre Bailon <abailon@baylibre.com>
2019-10-03 15:29:04 +0000

    Rename the product and use external SoC definitions
    The SoC definitions were defined here, in the product.
    In order to make easier to start a new project for that SoC,
    move all files specific to that SoC in common folder,
    and include them from the product.
    In addition, this rename the product with the name of the board.
    
    Signed-off-by: Alexandre Bailon <abailon@baylibre.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -14,41 +14,7 @@
 # limitations under the License.
 #
 
-# Use the non-open-source parts, if they're present
--include vendor/mediatek/mt8167/BoardConfigVendor.mk
-
-# Primary Arch
-TARGET_ARCH := arm64
-TARGET_ARCH_VARIANT := armv8-a
-TARGET_CPU_ABI := arm64-v8a
-TARGET_CPU_ABI2 :=
-TARGET_CPU_VARIANT := cortex-a53
-
-# Secondary Arch
-TARGET_2ND_ARCH := arm
-TARGET_2ND_ARCH_VARIANT := armv8-a
-TARGET_2ND_CPU_ABI := armeabi-v7a
-TARGET_2ND_CPU_ABI2 := armeabi
-TARGET_2ND_CPU_VARIANT := cortex-a53
-
-TARGET_IS_64_BIT := true
-TARGET_USES_64_BIT_BINDER := true
-
-# Enable vndk
-BOARD_VNDK_VERSION := current
-
-# bootimage generation
-BOARD_KERNEL_BASE = 0x40000000
-BOARD_KERNEL_OFFSET = 0x00080000
-BOARD_RAMDISK_OFFSET = 0x15000000
-BOARD_TAGS_OFFSET = 0x14000000
-
-BOARD_MKBOOTIMG_ARGS := \
-  --base $(BOARD_KERNEL_BASE) \
-  --kernel_offset $(BOARD_KERNEL_OFFSET) \
-  --ramdisk_offset $(BOARD_RAMDISK_OFFSET) \
-  --tags_offset $(BOARD_TAGS_OFFSET) \
-  --header_version 1
+include device/mediatek/common/soc/mt8167/BoardConfig.mk
 
 # kernel commandline
 BOARD_KERNEL_CMDLINE := \
@@ -74,21 +40,8 @@ BOARD_DTBOIMG_PARTITION_SIZE       := 4194304
 
 BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
 
-# prebuild dtbo file
-BOARD_PREBUILT_DTBOIMAGE := device/mediatek/common-kernel/dtbo.img
-
-# Use mke2fs to create ext4 images
-TARGET_USES_MKE2FS := true
-
-BOARD_SEPOLICY_DIRS += \
-        device/mediatek/mt8167/sepolicy/ \
-        device/mediatek/common/sepolicy/vendor \
-
-DEVICE_MANIFEST_FILE := device/mediatek/mt8167/manifest.xml
-
 # enable AVB
 BOARD_AVB_ENABLE := true
 BOARD_AVB_ALGORITHM := SHA512_RSA4096
 
-BOARD_VENDOR_MEDIATEK := true
-MTK_PARTITIONS_YAML := device/mediatek/mt8167/partitions.yaml
+MTK_PARTITIONS_YAML := device/mediatek/pumpkin/partitions.yaml

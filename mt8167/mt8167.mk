07ecf3df79a513275841ef98ac0fb9c989b1b42c
Mattijs Korpershoek <mkorpershoek@baylibre.com>
2019-11-18 17:00:12 -0800

    soc: mt8167: move u-boot copying to dedicated paragraph
    Separate this from the flashing image since it is usually customized
    
    Bug: https://baylibre.atlassian.net/browse/JUN-78
    Signed-off-by: Mattijs Korpershoek <mkorpershoek@baylibre.com>

--- mt8167/mt8167.mk
+++ mt8167/mt8167.mk
@@ -24,6 +24,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/binaries/images/dl_addr.ini:dl_addr.ini \
     $(LOCAL_PATH)/binaries/images/lk.bin:lk.bin \
-    $(LOCAL_PATH)/binaries/images/bl2.img:bl2.img \
+    $(LOCAL_PATH)/binaries/images/bl2.img:bl2.img
+
+# U-Boot and env
+PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/binaries/images/fip.bin:fip.bin \
     $(LOCAL_PATH)/binaries/images/u-boot-initial-env:u-boot-initial-env

b2d709a168ce7e29023bafbe481b2fac590c485c
Mattijs Korpershoek <mkorpershoek@baylibre.com>
2020-01-27 15:28:16 +0100

    overlay: add stay_on_while_plugged_in=1 settings
    Currently, we do not support any low power modes on Opal. Moreover, the
    bluetooth driver prevents from suspending anyways.
    
    To avoid any suspend issues, force Android to stay awake while being plugged.
    This is done via the settings overlay.
    
    Bug: RITA-133
    Signed-off-by: Mattijs Korpershoek <mkorpershoek@baylibre.com>

--- device.mk
+++ device.mk
@@ -21,6 +21,14 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_COPY_FILES += \
      device/mediatek/common/soc/mt8183/binaries/images/splashscreen.raw:splashscreen.raw
 
+# DEVICE_PACKAGE_OVERLAYS for the device should be before
+# including common overlays since the one listed first
+# takes precedence.
+ifdef DEVICE_PACKAGE_OVERLAYS
+$(warning Overlays defined in '$(DEVICE_PACKAGE_OVERLAYS)' will override '$(PRODUCT_HARDWARE)' overlays)
+endif
+DEVICE_PACKAGE_OVERLAYS += device/mediatek/opal/overlay
+
 # Additional hardware features
 $(call inherit-product-if-exists, vendor/mediatek/mt7668/mt7668.mk)
 # Touchscreen

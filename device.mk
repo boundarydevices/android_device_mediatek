88e3971bec07432d43a89bc6b57afd7fb99a4758
Phong LE <ple@baylibre.com>
2020-03-19 16:04:26 +0000

    onyx: enable ethernet
    Add and enable dhcpd, ethernet resource.
    
    Bug: https://baylibre.atlassian.net/browse/RITA-124
    Signed-off-by: Phong LE <ple@baylibre.com>

--- device.mk
+++ device.mk
@@ -17,6 +17,12 @@
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/init.onyx.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.onyx.rc \
 
+PRODUCT_PACKAGES += \
+    external/dhcpcd-6.8.2 \
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.ethernet.xml:system/etc/permissions/android.hardware.ethernet.xml
+
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.sf.lcd_density=180
 

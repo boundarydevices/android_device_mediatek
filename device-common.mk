426614c5682c179ba7415ced4754e2a1575b02ce
Mattijs Korpershoek <mkorpershoek@baylibre.com>
2020-05-11 17:08:54 +0200

    common: remove PRODUCT_SHIPPING_API_LEVEL variable
    Since this is a product variable, it should be set, per product.
    
    PRODUCT_SHIPPING_API_LEVEL is used by the Android build system for:
    - backward compatibility checks (Android Q on a product with shipping
    level = 28 is different than Android Q with shipping level = 29)
    
    Remove it from the common makefile
    
    Signed-off-by: Mattijs Korpershoek <mkorpershoek@baylibre.com>

--- device-common.mk
+++ device-common.mk
@@ -20,9 +20,6 @@ else
 LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
 endif
 
-# Shipping API level to Android P (28)
-PRODUCT_SHIPPING_API_LEVEL := 28
-
 PRODUCT_COPY_FILES := \
 	$(LOCAL_KERNEL):kernel
 

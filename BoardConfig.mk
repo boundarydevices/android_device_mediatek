boundarydevices/android_device_mediatek

commit e8c3613b3277c346e1a97583bf9a09adb4550ab8

BoardConfig: add dtbo support
Set the list of DTB files to be included in boot.img, and the list of
DTBO files to be included in dtbo.img

RITA-338

Signed-off-by: Amjad Ouled-Ameur <aouledameur@baylibre.com>
(cherry picked from commit b7c602eb9155c03dde9bb822991959d6a389d0b7)
Signed-off-by: Mattijs Korpershoek <mkorpershoek@baylibre.com>

BoardConfig.mk
@@ -43,6 +43,16 @@ BOARD_SEPOLICY_DIRS += \
     device/mediatek/quartz/sepolicy
 
 MTK_PARTITIONS_YAML := device/mediatek/quartz/partitions.yaml
+ 
+# Please keep this list fixed: add new files in the end of the list
+DTB_FILES := \
+        $(LOCAL_DTB)/mt8183-pumpkin.dtb
+
+# Please keep this list fixed: add new files in the end of the list
+DTBO_FILES := \
+        $(LOCAL_DTB)/mt8183-pumpkin-android.dtb \
+        $(LOCAL_DTB)/mt8183-pumpkin-urt-umo-9465md-t.dtb
+
 
 # Additional hardware features
 # NOTE: must be called last, as they append Board variables such

commit 39d170d0e7599e0f78686cc0983bb3da0077eba5

overlay: add missing ar0330 overlay to DTBO_FILES
With the introduction of DTB{,O}_FILES in commit
e8c3613b3277 ("BoardConfig: add dtbo support"), we forgot to add the
ar0330 overlay file.

Add it back to DTBO_FILES.

To use this, flash as following:
    ./flashimage.py --update dtbo_index 2

Fixes: e8c3613b3277 ("BoardConfig: add dtbo support")
Signed-off-by: Mattijs Korpershoek <mkorpershoek@baylibre.com>

BoardConfig.mk
@@ -51,8 +51,8 @@ DTB_FILES := \
 # Please keep this list fixed: add new files in the end of the list
 DTBO_FILES := \
         $(LOCAL_DTB)/mt8183-pumpkin-android.dtb \
-        $(LOCAL_DTB)/mt8183-pumpkin-urt-umo-9465md-t.dtb
-
+        $(LOCAL_DTB)/mt8183-pumpkin-urt-umo-9465md-t.dtb \
+        $(LOCAL_DTB)/mt8183-pumpkin-ar0330.dtb
 
 # Additional hardware features
 # NOTE: must be called last, as they append Board variables such
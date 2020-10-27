f2d30aef0f80785af99d8123cf1688e8c89714ea
Mattijs Korpershoek <mkorpershoek@baylibre.com>
2020-10-27 10:55:37 +0000

    audio: switch to TinyHal
    Currently, we use an internally developed Audio HAL which is compatible
    with the XML syntax of TinyHAL.
    
    Instead of maintaining this HAL, it would be a lot easier if we
    switched to the TinyHAL.
    
    Because of how TinyHAL looks up its configuration file
    (by using ro.product.device), we have to copy its configuration here
    instead of in the common soc files.
    
    Signed-off-by: Mattijs Korpershoek <mkorpershoek@baylibre.com>

--- device.mk
+++ device.mk
@@ -21,6 +21,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_COPY_FILES += \
      device/mediatek/common/soc/mt8183/binaries/images/splashscreen.raw:splashscreen.raw
 
+# Audio: use default hal configuration with TinyHAL
+PRODUCT_PACKAGES += audio.primary.quartz
+PRODUCT_COPY_FILES += \
+     device/mediatek/common/soc/mt8183/audio_xml/audio_hal_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio.quartz.xml
+
 # Touchscreen: add 90CCW rotation config file
 PRODUCT_COPY_FILES += \
      device/mediatek/quartz/input-manager-state.xml:$(TARGET_COPY_OUT_DATA)/system/input-manager-state.xml

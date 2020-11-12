2986405bca9ae7626ebce2f4065e6d8bb813eefc
Mattijs Korpershoek <mkorpershoek@baylibre.com>
2020-11-12 15:53:21 +0000

    wifi: remove all wifi related packages from device-common
    All WiFi related packages are defined in:
        vendor/mediatek/mt7668/mt7668.mk
    
    The ones defined here:
    - are outdated
    - actually break the WiFi feature by enforcing the usage of the optional
      vendor WiFi HAL (android.hardware.wifi@1.0-service)
    
    Remove the wifi related section
    
    Link: https://source.android.com/devices/tech/connect/wifi-hal#vendor_hal
    Signed-off-by: Mattijs Korpershoek <mkorpershoek@baylibre.com>

--- device-common.mk
+++ device-common.mk
@@ -73,26 +73,6 @@ PRODUCT_PACKAGES += memtrack.default \
     android.hardware.memtrack@1.0-service \
     android.hardware.memtrack@1.0-impl
 
-# Wifi
-PRODUCT_PACKAGES += \
-    libwpa_client \
-    wpa_supplicant \
-    hostapd \
-    wificond \
-    wifilogd \
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    wifi.interface=wlan0 \
-    wifi.supplicant_scan_interval=15 \
-
-PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service \
-    android.hardware.wifi@1.0 \
-    wifi-service \
-
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.wifi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.xml \
-
 # Usb
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \

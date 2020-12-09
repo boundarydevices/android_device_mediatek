c7e9df9c5c4ad1a6f82e61430e2ebe9513859924
Mattijs Korpershoek <mkorpershoek@baylibre.com>
2020-12-09 19:08:21 +0100

    utils: merge into common/soc repository
    We currently maintain this into a seperate git project, which is
    un-necessary.
    
    Move it here.
    
    This is based on the following repository:
    
    git : git@gitlab.com:baylibre/aosp/mediatek/product/common/utils.git
    commit : b84708af5c0ef2e939b8a1cf24731fe910f1ef25
    
        Mattijs Korpershoek (4):
              utils: debug: add stressapptest to default product packages
              init.userdebug: mount debugfs on early-init
              audio: add tiny{mix,play,cap} to userdebug builds
              utils: remove vendor ueventd merging from utils.mk
    
        Nguyen Du Phong LE (1):
              Initial commit
    
        Phong LE (1):
              utils: transfer to new repository
    
    Signed-off-by: Mattijs Korpershoek <mkorpershoek@baylibre.com>

---
+++ utils/utils.mk
@@ -0,0 +1,19 @@
+# Warning: this is actually a product definition, to be inherited from
+# To add this to board, add this to your <product>.mk:
+#     $(call inherit-product, device/mediatek/common/utils/utils.mk)
+
+UTILS_PATH := device/mediatek/common/utils
+
+ifneq (,$(filter userdebug eng,$(TARGET_BUILD_VARIANT)))
+PRODUCT_COPY_FILES += \
+    $(UTILS_PATH)/init.mediatek.userdebug.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.mediatek.userdebug.rc
+endif
+
+PRODUCT_PACKAGES_DEBUG += \
+    stressapptest
+
+# Low level audio tools for debugging
+PRODUCT_PACKAGES_DEBUG += \
+    tinymix \
+    tinyplay \
+    tinycap

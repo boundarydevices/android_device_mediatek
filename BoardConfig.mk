10e672ca837fad92b0b5d66a1058e5a8df1453c2
Phi-Bang Nguyen <pnguyen@baylibre.com>
2021-01-08 16:20:59 +0000

    BoardConfig: overlay: ar0330: Add the dual sensors overlay
    Since the dual ar0330 sensors config has a separate dt overlay file,
    add it into the BoardConfig.
    
    Signed-off-by: Phi-Bang Nguyen <pnguyen@baylibre.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -68,7 +68,8 @@ DTBO_FILES := \
         $(LOCAL_DTB)/mt8183-pumpkin-android.dtb \
         $(LOCAL_DTB)/mt8183-pumpkin-urt-umo-9465md-t.dtb \
         $(LOCAL_DTB)/mt8183-pumpkin-ar0330-single.dtb \
-        $(LOCAL_DTB)/mt8183-pumpkin-ap1302-ar0330.dtb
+        $(LOCAL_DTB)/mt8183-pumpkin-ap1302-ar0330.dtb \
+        $(LOCAL_DTB)/mt8183-pumpkin-ar0330-dual.dtb
 
 # Additional hardware features
 # NOTE: must be called last, as they append Board variables such

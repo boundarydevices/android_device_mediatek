d48804aea5a779792bd04f6c6ac92dc46429df63
Phi-Bang Nguyen <pnguyen@baylibre.com>
2021-01-22 10:04:33 +0000

    soc: mt8183: camera: Add the camera demo app
    Add the camera demo app for demonstration purposes.
    
    Signed-off-by: Phi-Bang Nguyen <pnguyen@baylibre.com>

--- mt8183/mt8183.mk
+++ mt8183/mt8183.mk
@@ -120,6 +120,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service-mtk \
     camera.libcamera \
     cam \
+    Camera2Video \
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.hardware.camera=libcamera

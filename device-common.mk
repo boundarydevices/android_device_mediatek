e310839dc6e9d95e7a8ce9b00c29f754b1a16824
Mattijs Korpershoek <mkorpershoek@baylibre.com>
2021-02-02 11:01:27 +0100

    common: add power hal service
    This is required for target level = 5 according to:
      //hardware/interfaces/compatibility_matrices/compatibility_matrix.5.xml
    
    Bug: RITA-456
    Test: atest VtsHalPowerTargetTest
    Signed-off-by: Mattijs Korpershoek <mkorpershoek@baylibre.com>

--- device-common.mk
+++ device-common.mk
@@ -102,6 +102,9 @@ PRODUCT_COPY_FILES += \
     device/mediatek/common/hal/media_xml/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml
 
+# Power HAL
+PRODUCT_PACKAGES += android.hardware.power-service.example
+
 # Add support for common utils: vendor ueventd.rc and (optional) debug tools
 $(call inherit-product, device/mediatek/common/soc/utils/utils.mk)
 

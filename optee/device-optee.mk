26ea9e6b8de8e2d66a93096fc7068d82fced4d5d
Guillaume La Roque <glaroque@baylibre.com>
2021-03-25 09:41:48 +0100

    build: add persist partition build script
    create persist parittion to store some persistent data
    
    Signed-off-by: Guillaume La Roque <glaroque@baylibre.com>

--- optee/device-optee.mk
+++ optee/device-optee.mk
@@ -13,6 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+PRODUCT_CUSTOM_IMAGE_MAKEFILES += \
+   device/mediatek/common/soc/build/custom_images/persist.mk
 
 # optee
 OPTEE_OS_DIR := external/optee-os

8a3e2bf192e723eb52aeae24ce7ca16244b72ebf
Mattijs Korpershoek <mkorpershoek@baylibre.com>
2021-04-19 16:52:51 +0200

    common: remove nonexistent composer@2.1-impl
    For hardware composer, the implementation is in drm_hwcomposer, not in
    android.hardware.graphics.composer@2.1-impl.
    
    Remove the nonexistent lib to avoid confusion
    
    Signed-off-by: Mattijs Korpershoek <mkorpershoek@baylibre.com>

--- device-common.mk
+++ device-common.mk
@@ -64,7 +64,6 @@ endif
 
 # 3D CPU renderer
 PRODUCT_PACKAGES += \
-    android.hardware.graphics.composer@2.1-impl \
     android.hardware.graphics.composer@2.1-service \
     android.hardware.graphics.mapper@2.0-impl \
     android.hardware.graphics.allocator@2.0-service \

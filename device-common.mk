fc8ae8ed06aa67d304f6c2a507cf660a65d128bb
Guillaume La Roque <glaroque@baylibre.com>
2021-05-05 08:38:43 +0200

    soc: vulkan: fix testVulkanDeqpLevel cts test
    copy missing xml file to fix CTS test :
    android.graphics.cts.VulkanDeqpLevelTest#testVulkanDeqpLevel
    
    Bug: RITA-516
    Signed-off-by: Guillaume La Roque <glaroque@baylibre.com>

--- device-common.mk
+++ device-common.mk
@@ -109,7 +109,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.vulkan.version-1_1.xml:vendor/etc/permissions/android.hardware.vulkan.version.xml \
     frameworks/native/data/etc/android.hardware.vulkan.compute-0.xml:vendor/etc/permissions/android.hardware.vulkan.compute.xml \
-    frameworks/native/data/etc/android.hardware.vulkan.level-1.xml:vendor/etc/permissions/android.hardware.vulkan.level.xml
+    frameworks/native/data/etc/android.hardware.vulkan.level-1.xml:vendor/etc/permissions/android.hardware.vulkan.level.xml \
+    frameworks/native/data/etc/android.software.vulkan.deqp.level-2020-03-01.xml:vendor/etc/permissions/android.software.vulkan.deqp.level.xml
+
 
 # RecoveryOS
 PRODUCT_COPY_FILES += \

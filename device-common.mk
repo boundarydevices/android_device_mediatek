458a16528b2ab1990bf46d64bf900c43e4f4eed7
Amjad Ouled-Ameur <aouledameur@baylibre.com>
2021-06-25 10:00:11 +0000

    CTS: add android.software.cts feature
    This is needed to grant some CTS permissions such us:
    - android.content.cts.REQUIRED_FEATURE_DEFINED
    - android.content.cts.REQUIRED_MULTI_GRANT
    
    RITA-539
    
    Signed-off-by: Amjad Ouled-Ameur <aouledameur@baylibre.com>

--- device-common.mk
+++ device-common.mk
@@ -82,6 +82,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:vendor/etc/r_submix_audio_policy_configuration.xml \
     frameworks/av/media/libeffects/data/audio_effects.xml:vendor/etc/audio_effects.xml
 
+# CTS
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.cts.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.cts.xml
+
 # Memtrack
 PRODUCT_PACKAGES += memtrack.default \
     android.hardware.memtrack@1.0-service \

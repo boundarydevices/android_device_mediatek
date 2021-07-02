91927d6bbc8cea302e70bfcf2523c3fe853c36c9
Amjad Ouled-Ameur <aouledameur@baylibre.com>
2021-07-02 09:28:10 +0000

    common: enable screen features
    Enable these two screen features needed for CTS compliance:
    - android.hardware.screen.portrait
    - android.hardware.screen.landscape
    
    RITA-548
    
    Signed-off-by: Amjad Ouled-Ameur <aouledameur@baylibre.com>

--- device-common.mk
+++ device-common.mk
@@ -112,6 +112,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.app_widgets.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.app_widgets.xml \
 
+# Screen features
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.screen.portrait.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.screen.portrait.xml \
+    frameworks/native/data/etc/android.hardware.screen.landscape.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.screen.landscape.xml
+
 # Vulkan
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.vulkan.version-1_1.xml:vendor/etc/permissions/android.hardware.vulkan.version.xml \

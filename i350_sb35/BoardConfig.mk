fcc1d9cae70727283fbd8bd2df735410b637966a
Mattijs Korpershoek <mkorpershoek@baylibre.com>
2021-07-16 15:41:46 +0200

    common: device/mediatek/common-kernel -> device/mediatek/kernel-binaries
    The kernel binaries repo, historically named common-kernel has been
    renamed to a more explicit name: kernel-binaries.
    
    Apply the rename here.
    
    Note: the folder stays in device/mediatek to mimic how other AOSP boards
    handle their binaries.
    
    Signed-off-by: Mattijs Korpershoek <mkorpershoek@baylibre.com>

--- i350_sb35/BoardConfig.mk
+++ i350_sb35/BoardConfig.mk
@@ -26,12 +26,12 @@ BOARD_KERNEL_CMDLINE := \
     androidboot.boot_devices=soc/11230000.mmc
 
 BOARD_VENDOR_KERNEL_MODULES += \
-    device/mediatek/common-kernel/$(TARGET_KERNEL_USE)/lt9611.ko \
-    device/mediatek/common-kernel/$(TARGET_KERNEL_USE)/panel-rpi-pumpkin-touchscreen.ko
+    device/mediatek/kernel-binaries/$(TARGET_KERNEL_USE)/lt9611.ko \
+    device/mediatek/kernel-binaries/$(TARGET_KERNEL_USE)/panel-rpi-pumpkin-touchscreen.ko
 
 BOARD_RECOVERY_KERNEL_MODULES += \
-    device/mediatek/common-kernel/$(TARGET_KERNEL_USE)/lt9611.ko \
-    device/mediatek/common-kernel/$(TARGET_KERNEL_USE)/panel-rpi-pumpkin-touchscreen.ko
+    device/mediatek/kernel-binaries/$(TARGET_KERNEL_USE)/lt9611.ko \
+    device/mediatek/kernel-binaries/$(TARGET_KERNEL_USE)/panel-rpi-pumpkin-touchscreen.ko
 
 # FS configuration
 ifeq ($(TARGET_USE_AB_SLOT), true)

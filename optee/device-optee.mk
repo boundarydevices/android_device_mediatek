d2174f36e9ea2379bfa6fef77530f18e91d4dc06
Julien Masson <jmasson@baylibre.com>
2021-07-27 19:39:28 +0200

    optee: add large TA
    This TA is to run xtest 1034.
    
    Signed-off-by: Julien Masson <jmasson@baylibre.com>
    (cherry picked from commit f1f5140e02769d882995608b322d26f129917d57)
    Signed-off-by: Mattijs Korpershoek <mkorpershoek@baylibre.com>

--- optee/device-optee.mk
+++ optee/device-optee.mk
@@ -82,6 +82,7 @@ PRODUCT_PACKAGES_DEBUG += a4c04d50-f180-11e8-8eb2-f2801f1b9fd1.ta # sims_keepali
 PRODUCT_PACKAGES_DEBUG += 614789f2-39c0-4ebf-b235-92b32ac107ed.ta # sha_perf
 PRODUCT_PACKAGES_DEBUG += 528938ce-fc59-11e8-8eb2-f2801f1b9fd1.ta # miss
 PRODUCT_PACKAGES_DEBUG += e6a33ed4-562b-463a-bb7e-ff5e15a493c8.ta # sims
+PRODUCT_PACKAGES_DEBUG += 25497083-a58a-4fc5-8a72-1ad7b69b8562.ta # large
 
 # tee-supplicant test plugin
 PRODUCT_PACKAGES_DEBUG += f07bfc66-958c-4a15-99c0-260e4e7375dd.plugin

4fb5e0ac4c04b86ad7928104cd99abb528e0675f
Amjad Ouled-Ameur <aouledameur@baylibre.com>
2021-08-30 12:02:54 +0000

    i500_pumpkin: Revert "touchsreen: add calibration file to rotate 90° CCW"
    This reverts commit 470b4679c69f543f9182f7d8b509aeffec89cc20.
    
    Enabling file-based encryption requires /data/system path to be empty.
    this is not currently the case because "input-manager-state.xml" is
    present in that path.
    
    Remove input-manager-state.xml from /data/system. Regarding the
    touchscreen calibration, it should be done from the device's dtbo.
    
    RITA-530
    
    Signed-off-by: Amjad Ouled-Ameur <aouledameur@baylibre.com>

--- i500_pumpkin/device.mk
+++ i500_pumpkin/device.mk
@@ -29,10 +29,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
      device/mediatek/board/i500_pumpkin/ILI210x_Touchscreen.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/ILI210x_Touchscreen.idc
 
-# Touchscreen: add 90CCW rotation config file
-PRODUCT_COPY_FILES += \
-     device/mediatek/board/i500_pumpkin/input-manager-state.xml:$(TARGET_COPY_OUT_DATA)/system/input-manager-state.xml
-
 # Key Layout files
 PRODUCT_COPY_FILES += \
      device/mediatek/board/i500_pumpkin/mtk-pmic-keys.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/mtk-pmic-keys.kl

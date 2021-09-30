d975cc406bbf583e3f0076a4acdfae1e03c6e47a
Mattijs Korpershoek <mkorpershoek@baylibre.com>
2021-09-30 09:10:12 +0200

    i350_pumpkin: add initial directory for i350_pumpkin
    This has been copied over from i350_evk based on commit:
    89817ae ("docs: rename rity-tools to aiot-tools for flashing commands")
    
    Signed-off-by: Mattijs Korpershoek <mkorpershoek@baylibre.com>

---
+++ i350_pumpkin/i350_evk.mk
@@ -0,0 +1,31 @@
+#
+# Copyright 2021 BayLibre SAS
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+
+# All OP-TEE vars MUST be set before including common/mt*
+OPTEE_ENABLE := true
+TEE_KEYMASTER_GATEKEEPER_ENABLE := true
+
+$(call inherit-product, device/mediatek/common/mt8365/mt8365.mk)
+
+PRODUCT_NAME := i350_evk
+PRODUCT_DEVICE := i350_evk
+PRODUCT_BRAND := Android
+PRODUCT_MODEL := i350_evk
+PRODUCT_MANUFACTURER := mediatek
+
+$(call inherit-product, device/mediatek/board/i350_evk/device.mk)

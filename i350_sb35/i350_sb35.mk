b47c29737a2b11fefa5e12fd63a3d7d2213c7ce5
Guillaume La Roque <glaroque@baylibre.com>
2021-10-12 09:26:31 +0200

    i350_sb35: select bootloaders with TARGET_BUILD_VARIANT
    link to bootloaders build env rework to support build mode,
    select bootloaders binaries automaticaly.
    debug when is a userdebug|eng build and release when it's a user
    build
    
    Signed-off-by: Guillaume La Roque <glaroque@baylibre.com>

--- i350_sb35/i350_sb35.mk
+++ i350_sb35/i350_sb35.mk
@@ -22,23 +22,25 @@ TEE_KEYMASTER_GATEKEEPER_ENABLE := true
 
 $(call inherit-product, device/mediatek/common/mt8365/mt8365.mk)
 
+include device/mediatek/common/common-build-flags.mk
+
 TARGET_USE_PRODUCT_SPECIFIC_BL2 := true
 TARGET_USE_PRODUCT_SPECIFIC_LK := true
 TARGET_USE_PRODUCT_SPECIFIC_UBOOT := true
 PRODUCT_COPY_FILES += \
-    device/mediatek/board/i350_sb35/binaries/images/lk.bin:lk.bin \
+    device/mediatek/board/i350_sb35/binaries/images/lk-$(TARGET_MODE_BL).bin:lk.bin \
     device/mediatek/common/mt8365/binaries/images/dl_addr.ini:dl_addr.ini
 
 ifeq ($(TARGET_USE_AB_SLOT), true)
 PRODUCT_COPY_FILES += \
-    device/mediatek/board/i350_sb35//binaries/images/fip_ab.bin:$(TARGET_OUT)/fip.bin \
-    device/mediatek/board/i350_sb35//binaries/images/bl2.img:$(TARGET_OUT)/bl2.img \
-    device/mediatek/board/i350_sb35//binaries/images/u-boot-initial-env_ab:$(TARGET_OUT)/u-boot-initial-env
+    device/mediatek/board/i350_sb35//binaries/images/fip_$(TARGET_MODE_BL)_ab.bin:$(TARGET_OUT)/fip.bin \
+    device/mediatek/board/i350_sb35//binaries/images/bl2-$(TARGET_MODE_BL).img:$(TARGET_OUT)/bl2.img \
+    device/mediatek/board/i350_sb35//binaries/images/u-boot-initial-$(TARGET_MODE_BL)-env_ab:$(TARGET_OUT)/u-boot-initial-env
 else
 PRODUCT_COPY_FILES += \
-    device/mediatek/board/i350_sb35/binaries/images/fip_noab.bin:$(TARGET_OUT)/fip.bin \
-    device/mediatek/board/i350_sb35/binaries/images/bl2.img:$(TARGET_OUT)/bl2.img \
-    device/mediatek/board/i350_sb35/binaries/images/u-boot-initial-env_noab:$(TARGET_OUT)/u-boot-initial-env
+    device/mediatek/board/i350_sb35/binaries/images/fip_$(TARGET_MODE_BL)_noab.bin:$(TARGET_OUT)/fip.bin \
+    device/mediatek/board/i350_sb35/binaries/images/bl2-$(TARGET_MODE_BL).img:$(TARGET_OUT)/bl2.img \
+    device/mediatek/board/i350_sb35/binaries/images/u-boot-initial-$(TARGET_MODE_BL)-env_noab:$(TARGET_OUT)/u-boot-initial-env
 endif # eq $(TARGET_USE_AB_SLOT), true
 
 

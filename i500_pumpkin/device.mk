boundarydevices/android_device_mediatek

commit 462839f8983e4c13176afdb4f85de0ee5bc28c71

i500_pumpkin: camera: use OnSemi GitHub for fw files
Currently, we are using an internal mirror for OnSemi camera firmwares.

This mirror is not maintained and OnSemi wants to have distribution
rights for their firmware files.

Use their public GitHub instead.

Bug: AIOT-359
Signed-off-by: Mattijs Korpershoek <mkorpershoek@baylibre.com>

i500_pumpkin/device.mk
@@ -33,6 +33,12 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
      device/mediatek/board/i500_pumpkin/mtk-pmic-keys.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/mtk-pmic-keys.kl
 
+# Camera firmwares
+PRODUCT_COPY_FILES := \
+    external/onsemi/OLogic_Pumpkin_i500//ap1302_ar0330_single_fw.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/ap1302_ar0330_single_fw.bin \
+    external/onsemi/OLogic_Pumpkin_i500//ap1302_ar0144_single_fw.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/ap1302_ar0144_single_fw.bin \
+    external/onsemi/OLogic_Pumpkin_i500//ap1302_ar0144_dual_fw.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/ap1302_ar0144_dual_fw.bin
+
 # Shipping API level to Android R (30)
 PRODUCT_SHIPPING_API_LEVEL := 30
 
@@ -52,8 +58,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 $(call inherit-product-if-exists, vendor/mediatek/wireless/mt7668.mk)
 # Touchscreen
 $(call inherit-product, vendor/ilitek/ili251x.mk)
-# Camera
-$(call inherit-product-if-exists, vendor/onsemi/ap1302/ap1302.mk)
 # UVC camera
 $(call inherit-product, hardware/mediatek/camera/uvc/uvc.mk)
 # CSI camera

i500_pumpkin/BoardConfig.mk
@@ -25,6 +25,10 @@ BOARD_KERNEL_CMDLINE := \
     androidboot.boot_devices=soc/11230000.mmc \
     mem_sleep_default=s2idle
 
+# kernel modules required for ap1302 isp
+BOARD_VENDOR_KERNEL_MODULES += \
+    device/mediatek/kernel-binaries/$(TARGET_KERNEL_USE)/ap1302.ko
+
 # FS configuration
 ifeq ($(TARGET_USE_AB_SLOT), true)
 BOARD_SUPER_PARTITION_SIZE         := 4831838208
@@ -82,8 +86,6 @@ DTBO_FILES := \
 -include vendor/mediatek/wireless/BoardConfig-mt7668.mk
 # Touchscreen
 include vendor/ilitek/BoardConfig-ili251x.mk
-# Camera
--include vendor/onsemi/ap1302/BoardConfig-ap1302.mk
 # UVC Camera
 include hardware/mediatek/camera/uvc/BoardConfig.mk
 # CSI Camera

commit ab0bca502935f8e8ee0699015a9f52a0d2c197db

i500_pumpkin: fix missing files in filesystem and OUT_DIR
onsemi firmware support introduce regression:
PRODUCT_COPY_FILES := instead of +=

Fixes: 462839f ("i500_pumpkin: camera: use OnSemi GitHub for fw files")
Signed-off-by: Guillaume La Roque <glaroque@baylibre.com>

i500_pumpkin/device.mk
@@ -34,7 +34,7 @@ PRODUCT_COPY_FILES += \
      device/mediatek/board/i500_pumpkin/mtk-pmic-keys.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/mtk-pmic-keys.kl
 
 # Camera firmwares
-PRODUCT_COPY_FILES := \
+PRODUCT_COPY_FILES += \
     external/onsemi/OLogic_Pumpkin_i500//ap1302_ar0330_single_fw.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/ap1302_ar0330_single_fw.bin \
     external/onsemi/OLogic_Pumpkin_i500//ap1302_ar0144_single_fw.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/ap1302_ar0144_single_fw.bin \
     external/onsemi/OLogic_Pumpkin_i500//ap1302_ar0144_dual_fw.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/ap1302_ar0144_dual_fw.bin
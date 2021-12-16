6a6661f3331f77e42673c9e9bb0231d12198fe6f
Mattijs Korpershoek <mkorpershoek@baylibre.com>
2021-12-16 09:36:22 +0100

    common: upgrade android.hardware.drm@1.3-service.clearkey to 1.4
    When build android 12, we have the following build error:
    
    build/make/core/main.mk:1294: warning:  device/mediatek/board/i500_pumpkin/i500_pumpkin.mk includes non-existent modules in PRODUCT_PACKAGES
    Offending entries:
    android.hardware.drm@1.3-service.clearkey
    
    Fix it by upgrading the clearkey drm service.
    
    Bug: RITA-901
    Signed-off-by: Mattijs Korpershoek <mkorpershoek@baylibre.com>

--- device-common.mk
+++ device-common.mk
@@ -111,7 +111,7 @@ PRODUCT_PACKAGES += memtrack.default \
 
 # DRM (Digital Rights Management)
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.3-service.clearkey
+    android.hardware.drm@1.4-service.clearkey
 
 # USB
 PRODUCT_PACKAGES += \

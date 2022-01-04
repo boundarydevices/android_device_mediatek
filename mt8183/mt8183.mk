d4b233366c08418b39a08c977e95adf46705d185
Julien STEPHAN <jstephan@baylibre.com>
2022-01-04 09:46:48 +0100

    Revert "REVERTME: mt8183: disable armnn HAL for Android 12 bringup"
    This reverts commit f3ca1dfe9a4a39a63fff2657eb5fe42685fb1162.

--- mt8183/mt8183.mk
+++ mt8183/mt8183.mk
@@ -125,9 +125,8 @@ PRODUCT_PACKAGES +=  vulkan.mali
 VENDOR_UEVENTD_FILES += \
     vendor/mediatek/prebuilts/egl/mali/ueventd.rc
 
-# FIXME: disabled for Android 12 bringup
-# PRODUCT_PACKAGES += \
-#     android.hardware.neuralnetworks@1.3-service-armnn
+PRODUCT_PACKAGES += \
+    android.hardware.neuralnetworks@1.3-service-armnn
 
 PRODUCT_PROPERTY_OVERRIDES += \
     Armnn.operandTypeTensorQuant8AsymmPerformance.execTime=2 \

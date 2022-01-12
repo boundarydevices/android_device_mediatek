ba5dd885208166d59125a1501979665b57aa98da
fchiby <fchiby@baylibre.com>
2022-01-12 18:25:29 +0000

    build: fix mtk-dist for android12
    cache.img and recovery.img are generated just in case "TARGET_USE_AB_SLOT=false"
    
    SOONG_ZIP usage has been updated in Android12:
       "-f" was added as a prefix to each file to include it in the zip package.
       "-C $(dir $<)" was added to specify directory.
    
    code cleanup by reducing extra spaces/tabs and removing empty lines
    
    BUG: RITA-934
    
    Signed-off-by: fchiby <fchiby@baylibre.com>

--- build/tasks/mtk-dist.mk
+++ build/tasks/mtk-dist.mk
@@ -1,25 +1,29 @@
 #necessary files for aiot-flash
 INTERNAL_IMG_PACKAGE_FILES := \
-        $(PRODUCT_OUT)/bl2.img \
-        $(PRODUCT_OUT)/fip.bin \
-        $(PRODUCT_OUT)/flashproc.py \
-        $(PRODUCT_OUT)/lk.bin \
-        $(PRODUCT_OUT)/MBR_EMMC \
-        $(PRODUCT_OUT)/partitions.yaml \
-        $(PRODUCT_OUT)/persist.img \
-        $(PRODUCT_OUT)/splashscreen.raw \
-        $(PRODUCT_OUT)/u-boot-initial-env \
-        $(PRODUCT_OUT)/android-info.txt \
-        $(PRODUCT_OUT)/boot.img \
-        $(PRODUCT_OUT)/cache.img \
-        $(PRODUCT_OUT)/dtbo.img \
-        $(PRODUCT_OUT)/recovery.img \
-        $(PRODUCT_OUT)/super.img \
-        $(PRODUCT_OUT)/userdata.img \
+	$(PRODUCT_OUT)/bl2.img \
+	$(PRODUCT_OUT)/fip.bin \
+	$(PRODUCT_OUT)/flashproc.py \
+	$(PRODUCT_OUT)/lk.bin \
+	$(PRODUCT_OUT)/MBR_EMMC \
+	$(PRODUCT_OUT)/partitions.yaml \
+	$(PRODUCT_OUT)/persist.img \
+	$(PRODUCT_OUT)/splashscreen.raw \
+	$(PRODUCT_OUT)/u-boot-initial-env \
+	$(PRODUCT_OUT)/android-info.txt \
+	$(PRODUCT_OUT)/boot.img \
+	$(PRODUCT_OUT)/dtbo.img \
+	$(PRODUCT_OUT)/super.img \
+	$(PRODUCT_OUT)/userdata.img \
 
 ifeq ($(TARGET_AVB_ENABLE), true)
 INTERNAL_IMG_PACKAGE_FILES += \
-        $(PRODUCT_OUT)/vbmeta.img
+	$(PRODUCT_OUT)/vbmeta.img
+endif
+
+ifeq ($(TARGET_USE_AB_SLOT), false)
+INTERNAL_IMG_PACKAGE_FILES += \
+	$(PRODUCT_OUT)/cache.img \
+	$(PRODUCT_OUT)/recovery.img
 endif
 
 # The update package
@@ -31,8 +35,7 @@ name := $(TARGET_PRODUCT)$(dbg)-img-mtk-$(FILE_NAME_TAG)
 
 MTK_INTERNAL_UPDATE_PACKAGE_TARGET := $(PRODUCT_OUT)/$(name).zip
 $(MTK_INTERNAL_UPDATE_PACKAGE_TARGET): $(INTERNAL_IMG_PACKAGE_FILES)
-		$(info Package: $@)
-		$(hide) $(SOONG_ZIP) -o $@ $(addprefix -f ,$(notdir $^))
+	$(info Package: $@)
+	$(hide) $(SOONG_ZIP) -o $@ -C $(dir $<) $(addprefix -f ,$^)
 .PHONY: mtk-dist
 mtk-dist: $(MTK_INTERNAL_UPDATE_PACKAGE_TARGET)
-

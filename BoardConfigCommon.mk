40aa103ca4effdcc00013200b7dbaa29e8f561bb
Guillaume Ranquet <granquet@baylibre.com>
2022-04-04 19:57:41 +0200

    common: GKI: add preliminary GKI support
    Generic Kernel Image basic support.
    In this state, the GKI build boots up to the serial console.
    The partitions are mounted and the userland is started.
    
    The feature is controled by the MTK_USES_GKI build flag
    
    Signed-off-by: Guillaume Ranquet <granquet@baylibre.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -61,6 +61,55 @@ BOARD_RECOVERY_KERNEL_MODULES := \
     device/mediatek/kernel-binaries/$(TARGET_KERNEL_USE)/mediatek-drm.ko \
     device/mediatek/kernel-binaries/$(TARGET_KERNEL_USE)/mediatek-drm-hdmi.ko
 
+ifneq ($(MTK_USES_GKI),)
+
+# Built as module when using GKI
+BOARD_VENDOR_KERNEL_MODULES += \
+device/mediatek/kernel-binaries/$(TARGET_KERNEL_USE)/goodix.ko \
+device/mediatek/kernel-binaries/$(TARGET_KERNEL_USE)/phy-mtk-hdmi-drv.ko \
+device/mediatek/kernel-binaries/$(TARGET_KERNEL_USE)/phy-mtk-tphy.ko \
+device/mediatek/kernel-binaries/$(TARGET_KERNEL_USE)/pwm-mtk-disp.ko \
+device/mediatek/kernel-binaries/$(TARGET_KERNEL_USE)/snd-soc-mt6358.ko \
+device/mediatek/kernel-binaries/$(TARGET_KERNEL_USE)/mtk-kpd.ko \
+device/mediatek/kernel-binaries/$(TARGET_KERNEL_USE)/tcpci_mt6360.ko \
+device/mediatek/kernel-binaries/$(TARGET_KERNEL_USE)/v4l2-fwnode.ko \
+device/mediatek/kernel-binaries/$(TARGET_KERNEL_USE)/mtk-seninf.ko \
+device/mediatek/kernel-binaries/$(TARGET_KERNEL_USE)/phy-generic.ko \
+device/mediatek/kernel-binaries/$(TARGET_KERNEL_USE)/tusb322i.ko \
+device/mediatek/kernel-binaries/$(TARGET_KERNEL_USE)/mtu3.ko \
+device/mediatek/kernel-binaries/$(TARGET_KERNEL_USE)/xhci-mtk-hcd.ko \
+device/mediatek/kernel-binaries/$(TARGET_KERNEL_USE)/musb_hdrc.ko \
+device/mediatek/kernel-binaries/$(TARGET_KERNEL_USE)/mediatek.ko \
+
+# Necesasry modules to boot the board to the userland
+BOARD_VENDOR_RAMDISK_KERNEL_MODULES += \
+device/mediatek/kernel-binaries/$(TARGET_KERNEL_USE)/mt6397.ko \
+device/mediatek/kernel-binaries/$(TARGET_KERNEL_USE)/btmtkuart.ko \
+device/mediatek/kernel-binaries/$(TARGET_KERNEL_USE)/mediatek-cpufreq.ko \
+device/mediatek/kernel-binaries/$(TARGET_KERNEL_USE)/hwmon.ko \
+device/mediatek/kernel-binaries/$(TARGET_KERNEL_USE)/ntc_thermistor.ko \
+device/mediatek/kernel-binaries/$(TARGET_KERNEL_USE)/kfifo_buf.ko \
+device/mediatek/kernel-binaries/$(TARGET_KERNEL_USE)/nvmem_mtk-efuse.ko \
+device/mediatek/kernel-binaries/$(TARGET_KERNEL_USE)/regmap-spmi.ko \
+device/mediatek/kernel-binaries/$(TARGET_KERNEL_USE)/mt6311-regulator.ko \
+device/mediatek/kernel-binaries/$(TARGET_KERNEL_USE)/mt6315-regulator.ko \
+device/mediatek/kernel-binaries/$(TARGET_KERNEL_USE)/mt6323-regulator.ko \
+device/mediatek/kernel-binaries/$(TARGET_KERNEL_USE)/mt6358-regulator.ko \
+device/mediatek/kernel-binaries/$(TARGET_KERNEL_USE)/mt6380-regulator.ko \
+device/mediatek/kernel-binaries/$(TARGET_KERNEL_USE)/mt6397-regulator.ko \
+device/mediatek/kernel-binaries/$(TARGET_KERNEL_USE)/mtk_wdt.ko \
+device/mediatek/kernel-binaries/$(TARGET_KERNEL_USE)/cqhci.ko \
+device/mediatek/kernel-binaries/$(TARGET_KERNEL_USE)/mtk-sd.ko \
+device/mediatek/kernel-binaries/$(TARGET_KERNEL_USE)/mtk-svs.ko \
+device/mediatek/kernel-binaries/$(TARGET_KERNEL_USE)/matrix-keymap.ko \
+device/mediatek/kernel-binaries/$(TARGET_KERNEL_USE)/mtk-pmic-keys.ko \
+device/mediatek/kernel-binaries/$(TARGET_KERNEL_USE)/mtk-pmic-wrap.ko \
+# Uart still built-in for debug purposes, but long term it shall be built as modules
+#device/mediatek/kernel-binaries/$(TARGET_KERNEL_USE)/8250_mtk.ko \
+#device/mediatek/kernel-binaries/$(TARGET_KERNEL_USE)/mtk-uart-apdma.ko \
+
+endif
+
 # Use mke2fs to create ext4 images
 TARGET_USES_MKE2FS := true
 

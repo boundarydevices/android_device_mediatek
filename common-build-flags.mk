1cfa331500f77176ad014e3c2fc1e53c0d8912dc
Julien Masson <jmasson@baylibre.com>
2022-05-19 14:47:52 +0200

    common: provide copy_bl_binaries macro
    This macro handle the copy of bootloader binaries following the
    TARGET_MODE_BL.
    
    Example: device/mediatek/board/i500_pumpkin/i500_pumpkin.mk
    ```
    $(call copy_bl_binaries, device/mediatek/common/mt8183/binaries/images)
    ```
    
    Signed-off-by: Julien Masson <jmasson@baylibre.com>

--- common-build-flags.mk
+++ common-build-flags.mk
@@ -9,3 +9,15 @@ else
 TARGET_MODE_BL := debug
 endif
 endif
+
+define copy_bl_binaries
+$(eval PRODUCT_COPY_FILES += \
+           $(1)/bl2-$(TARGET_MODE_BL).img:$(TARGET_OUT)/bl2.img \
+           $(1)/fip_$(TARGET_MODE_BL).bin:$(TARGET_OUT)/fip.bin \
+           $(1)/lk-$(TARGET_MODE_BL).bin:$(TARGET_OUT)/lk.bin \
+           $(1)/u-boot-initial-$(TARGET_MODE_BL)-env:$(TARGET_OUT)/u-boot-initial-env) \
+$(if $(filter factory,$(TARGET_MODE_BL)),\
+  $(eval PRODUCT_COPY_FILES += \
+             $(1)/auth_sv5.auth:$(TARGET_OUT)/auth_sv5.auth \
+             $(1)/lk-$(TARGET_MODE_BL).sign:$(TARGET_OUT)/lk.sign))
+endef

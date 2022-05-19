c0924670c359a2ac6abd04827e59076cde4d6fca
Julien Masson <jmasson@baylibre.com>
2022-05-19 14:45:10 +0200

    common: support factory build
    TARGET_MODE_BL is now set to factory if the user set FACTORY_BUILD to
    true.
    The bootloader binaries generated for factory have the same
    conventional name as release|debug.
    
    Signed-off-by: Julien Masson <jmasson@baylibre.com>

--- common-build-flags.mk
+++ common-build-flags.mk
@@ -1,6 +1,10 @@
 ifndef TARGET_MODE_BL
 ifeq ($(TARGET_BUILD_VARIANT), user)
+ifeq ($(FACTORY_BUILD), true)
+TARGET_MODE_BL := factory
+else
 TARGET_MODE_BL := release
+endif
 else
 TARGET_MODE_BL := debug
 endif

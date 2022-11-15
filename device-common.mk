b4a6bac35ac7b68fee055296b9fe3572253096c0
Julien Masson <jmasson@baylibre.com>
2022-11-15 10:40:44 +0100

    Drop support of mt8167
    mt8167 platforms are not maintained anymore, all related configs can
    be removed.
    
    Signed-off-by: Julien Masson <jmasson@baylibre.com>

--- device-common.mk
+++ device-common.mk
@@ -75,7 +75,7 @@ PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-service.software
 endif
 
-# mt8183 and mt8167 both have the same OpenGL ES version (3.2)
+# mt8183 has OpenGL ES version (3.2)
 # this value should be moved in specific soc folder if it's not aligned in next deliveries
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.opengles.version=196610

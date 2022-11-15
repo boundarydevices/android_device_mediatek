29ebcf8f1d6eaf563f988be6fa5c8f18c432696f
Julien Masson <jmasson@baylibre.com>
2022-11-15 15:05:09 +0100

    Update kernel-binaries path to boards-binaries
    All kernel binaries are now generated in boards-binaries project.
    
    Signed-off-by: Julien Masson <jmasson@baylibre.com>

--- device-common.mk
+++ device-common.mk
@@ -22,7 +22,7 @@ endif
 PRODUCT_OTA_ENFORCE_VINTF_KERNEL_REQUIREMENTS := false
 
 ifeq ($(TARGET_PREBUILT_KERNEL),)
-LOCAL_KERNEL := device/mediatek/kernel-binaries/$(TARGET_KERNEL_USE)/Image
+LOCAL_KERNEL := device/mediatek/boards-binaries/$(TARGET_KERNEL_USE)/Image
 else
 LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
 endif

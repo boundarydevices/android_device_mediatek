4e8f18980dcc5ad01423f60fc4bba3701b836d27
Vitor Sato Eschholz <vsatoes@baylibre.com>
2023-04-19 06:46:12 +0000

    common-build-flags: change fip binary name
    Change the fip binary name to match the one generated by the build
    bootloaders scripts [1].
    
    [1]: https://gitlab.baylibre.com/baylibre/mediatek/bsp/build-bootloaders
    
    Signed-off-by: Vitor Sato Eschholz <vsatoes@baylibre.com>

--- common-build-flags.mk
+++ common-build-flags.mk
@@ -13,7 +13,7 @@ endif
 define copy_bl_binaries
 $(eval PRODUCT_COPY_FILES += \
            $(1)/bl2-$(TARGET_MODE_BL).img:$(TARGET_OUT)/bl2.img \
-           $(1)/fip_$(TARGET_MODE_BL).bin:$(TARGET_OUT)/fip.bin \
+           $(1)/fip-$(TARGET_MODE_BL).bin:$(TARGET_OUT)/fip.bin \
            $(1)/lk-$(TARGET_MODE_BL).bin:$(TARGET_OUT)/lk.bin \
            $(1)/u-boot-initial-$(TARGET_MODE_BL)-env:$(TARGET_OUT)/u-boot-initial-env) \
 $(if $(filter factory,$(TARGET_MODE_BL)),\

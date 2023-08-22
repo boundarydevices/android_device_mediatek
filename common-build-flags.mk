6ce0957380c244c0c1908d3788e8d3912dd33717
Julien Masson <jmasson@baylibre.com>
2023-08-22 11:56:13 +0200

    bootloaders: rename binaries
    With the recent updates from aiot-tools the bootloaders binaries have
    been renamed:
    - fip.bin -> bootloaders.img
    - bl2.img -> mmcboot.bin
    - lk.bin -> da.bin
    
    Signed-off-by: Julien Masson <jmasson@baylibre.com>

--- common-build-flags.mk
+++ common-build-flags.mk
@@ -12,12 +12,12 @@ endif
 
 define copy_bl_binaries
 $(eval PRODUCT_COPY_FILES += \
-           $(1)/bl2-$(TARGET_MODE_BL).img:$(TARGET_OUT)/bl2.img \
-           $(1)/fip-$(TARGET_MODE_BL).bin:$(TARGET_OUT)/fip.bin \
-           $(1)/lk-$(TARGET_MODE_BL).bin:$(TARGET_OUT)/lk.bin \
+           $(1)/bootloaders-$(TARGET_MODE_BL).img:$(TARGET_OUT)/bootloaders.img \
+           $(1)/da-$(TARGET_MODE_BL).bin:$(TARGET_OUT)/da.bin \
+           $(1)/mmcboot-$(TARGET_MODE_BL).bin:$(TARGET_OUT)/mmcboot.bin \
            $(1)/u-boot-initial-$(TARGET_MODE_BL)-env:$(TARGET_OUT)/u-boot-initial-env) \
 $(if $(filter factory,$(TARGET_MODE_BL)),\
   $(eval PRODUCT_COPY_FILES += \
              $(1)/auth_sv5.auth:$(TARGET_OUT)/auth_sv5.auth \
-             $(1)/lk-$(TARGET_MODE_BL).sign:$(TARGET_OUT)/lk.sign))
+             $(1)/da-$(TARGET_MODE_BL).sign:$(TARGET_OUT)/da.sign))
 endef

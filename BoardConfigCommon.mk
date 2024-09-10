76beab2f84b771e57e48758584aed599bcda7bd5
Julien Masson <jmasson@baylibre.com>
2024-09-10 10:12:03 +0200

    kernel: remove manifest_kernel5.xml
    Kernel 5.4 is not supported on Android 14, manifest_kernel5.xml can be
    removed from the project.
    
    Signed-off-by: Julien Masson <jmasson@baylibre.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -122,10 +122,6 @@ BOARD_SEPOLICY_DIRS := \
 BOARD_SEPOLICY_DIRS += \
         hardware/mediatek/usb/1.2/sepolicy
 
-ifeq ($(TARGET_KERNEL_USE), 5.4)
-DEVICE_MANIFEST_FILE += device/mediatek/common/manifest_kernel5.xml
-endif
-
 BOARD_VENDOR_MEDIATEK := true
 
 # Set location of DTB/DTBO files

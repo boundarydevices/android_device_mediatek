1b2a6b2ff5ea537e46611e170a4f2c7a5e89f0e5
Esteban Blanc <eblanc@baylibre.com>
2024-10-02 10:13:27 +0000

    i350_evk: move optee ta inclusion to common project
    In order to reuse the logic for other board the whole TA inclusion logic
    has been moved to common
    
    Signed-off-by: Esteban Blanc <eblanc@baylibre.com>

--- i350_evk/i350_evk.mk
+++ i350_evk/i350_evk.mk
@@ -20,6 +20,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 OPTEE_ENABLE := true
 TEE_KEYMASTER_GATEKEEPER_ENABLE := true
 
+BOOTLOADERS_BINARIES := device/mediatek/boards-binaries/i350_evk
+
 $(call inherit-product, device/mediatek/common/mt8365/mt8365.mk)
 
 PRODUCT_NAME := i350_evk
@@ -39,24 +41,3 @@ allowed_list := product_manifest.xml
 allowed_list += android.hardware.health@2.0-impl-default.recovery
 allowed_list += DeviceDiagnostics
 $(call enforce-product-packages-exist, $(allowed_list))
-
-# OP-TEE Trusted Applications
-ifeq ($(OPTEE_ENABLE), true)
-include device/mediatek/common/security/optee/device.mk
-
-I350_EVK_TA := device/mediatek/boards-binaries/i350_evk/optee-ta
-
-ifeq ($(TEE_KEYMASTER_GATEKEEPER_ENABLE), true)
-# gatekeeper
-$(call optee-add-ta, $(I350_EVK_TA)/4d573443-6a56-4272-ac6f-2425af9ef9bb.ta)
-# keymaster
-$(call optee-add-ta, $(I350_EVK_TA)/dba51a17-0563-11e7-93b1-6fa7b0071a51.ta)
-endif
-
-ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
-# supp_plugin
-$(call optee-add-ta, $(I350_EVK_TA)/380231ac-fb99-47ad-a689-9e017eb6e78a.ta)
-# xtest
-$(call optee-add-all-xtest-ta, $(I350_EVK_TA))
-endif
-endif

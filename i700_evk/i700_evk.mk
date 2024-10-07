a4c0a03fafa2567d672a7c0eda820b1b32ae3837
Esteban Blanc <eblanc@baylibre.com>
2024-10-07 13:15:05 +0000

    i700_evk: add basic support for i700_evk
    Add a basic support that goes up to the console
    
    Signed-off-by: Esteban Blanc <eblanc@baylibre.com>

---
+++ i700_evk/i700_evk.mk
@@ -0,0 +1,39 @@
+#
+# Copyright 2021 BayLibre SAS
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+
+# All OP-TEE vars MUST be set before including common/mt*
+OPTEE_ENABLE := false
+TEE_KEYMASTER_GATEKEEPER_ENABLE := false
+
+BOOTLOADERS_BINARIES := device/mediatek/boards-binaries/i700_evk
+
+$(call inherit-product, device/mediatek/common/mt8390/mt8390.mk)
+
+PRODUCT_NAME := i700_evk
+PRODUCT_DEVICE := i700_evk
+PRODUCT_BRAND := Android
+PRODUCT_MODEL := i700_evk
+PRODUCT_MANUFACTURER := mediatek
+
+$(call inherit-product, device/mediatek/boards/i700_evk/device.mk)
+
+# clean-up all unknown PRODUCT_PACKAGES
+allowed_list := product_manifest.xml
+allowed_list += android.hardware.health@2.0-impl-default.recovery
+allowed_list += DeviceDiagnostics
+$(call enforce-product-packages-exist, $(allowed_list))

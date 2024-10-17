51e25080401d7c74955ae6d40b72e3d4f4e97758
Vitor Sato Eschholz <vsatoes@baylibre.com>
2024-10-17 08:22:30 +0000

    i350_evk: BoardConfig: add hardware and serialno to cmdline
    Hardware and serialno bootargs are no longer managed by the
    bootloader. Set them using the kernel comand line.
    
    Signed-off-by: Vitor Sato Eschholz <vsatoes@baylibre.com>

--- i350_evk/BoardConfig.mk
+++ i350_evk/BoardConfig.mk
@@ -28,7 +28,10 @@ BOARD_KERNEL_CMDLINE := \
     firmware_class.path=/vendor/firmware \
     printk.devkmsg=on \
     init=/init \
-    androidboot.boot_devices=soc/11230000.mmc
+    androidboot.boot_devices=soc/11230000.mmc \
+    androidboot.hardware=mt8365 \
+    androidboot.serialno=i350evk
+
 
 # kernel modules required for ap1302 isp
 BOARD_VENDOR_KERNEL_MODULES += \

e414025a27321018a1f8a19193440838b35d4485
Esteban Blanc <eblanc@baylibre.com>
2024-10-25 11:05:06 +0000

    i700_evk: Enable optee and with TEE for keymint
    Now that we have TA built for i700, enabling optee and keymint works
    
    Signed-off-by: Esteban Blanc <eblanc@baylibre.com>

--- i700_evk/i700_evk.mk
+++ i700_evk/i700_evk.mk
@@ -17,8 +17,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 # All OP-TEE vars MUST be set before including common/mt*
-OPTEE_ENABLE := false
-TEE_KEYMASTER_GATEKEEPER_ENABLE := false
+OPTEE_ENABLE := true
+TEE_KEYMASTER_GATEKEEPER_ENABLE := true
 
 BOOTLOADERS_BINARIES := device/mediatek/boards-binaries/i700_evk
 

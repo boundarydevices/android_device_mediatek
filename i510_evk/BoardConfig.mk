ba531307362b594345ffcb3271a7ed58c1d0f9d2
Esteban Blanc <eblanc@baylibre.com>
2024-11-19 15:38:47 +0000

    i510_evk: add board support
    Add support for Genio 510 EVK based on the mt8370 SoC
    
    Signed-off-by: Esteban Blanc <eblanc@baylibre.com>

---
+++ i510_evk/BoardConfig.mk
@@ -0,0 +1,50 @@
+#
+# Copyright 2014 The Android Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+include device/mediatek/common/mt8188/BoardConfig.mk
+
+TARGET_BOOTLOADER_BOARD_NAME := i510_evk
+
+# kernel commandline
+BOARD_KERNEL_CMDLINE := \
+    firmware_class.path=/vendor/firmware \
+    printk.devkmsg=on \
+    init=/init \
+    androidboot.boot_devices=soc/11230000.mmc \
+    androidboot.hardware=mt8370 \
+    androidboot.serialno=i510evk \
+    androidboot.selinux=permissive
+
+# FS configuration
+# FIXME: This should be adjusted to i510_evk
+BOARD_SUPER_PARTITION_SIZE         := 4831838208
+TARGET_USERIMAGES_USE_EXT4         := true
+TARGET_COPY_OUT_VENDOR             := vendor
+BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_DTBOIMG_PARTITION_SIZE       := 4194304
+BOARD_BOOTIMAGE_PARTITION_SIZE     := 67108864
+BOARD_DB_DYNAMIC_PARTITIONS_SIZE   := 2411724800
+BOARD_USES_METADATA_PARTITION      := true
+
+MTK_PARTITIONS_YAML := device/mediatek/boards/i510_evk/partitions.yaml
+
+# Please keep this list fixed: add new files in the end of the list
+DTB_FILES := \
+    $(LOCAL_DTB)/mt8370-genio-510-evk.dtb
+
+# Please keep this list fixed: add new files in the end of the list
+DTBO_FILES := \
+    $(LOCAL_DTB)/mt8370-genio-510-evk-android.dtb
---
+++ i510_evk/i510_evk.mk
@@ -0,0 +1,39 @@
+#
+# Copyright 2024 BayLibre SAS
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+
+# All OP-TEE vars MUST be set before including common/mt*
+OPTEE_ENABLE := true
+TEE_KEYMASTER_GATEKEEPER_ENABLE := true
+
+BOOTLOADERS_BINARIES := device/mediatek/boards-binaries/i510_evk
+
+$(call inherit-product, device/mediatek/common/mt8188/mt8370.mk)
+
+PRODUCT_NAME := i510_evk
+PRODUCT_DEVICE := i510_evk
+PRODUCT_BRAND := Android
+PRODUCT_MODEL := i510_evk
+PRODUCT_MANUFACTURER := mediatek
+
+$(call inherit-product, device/mediatek/boards/i510_evk/device.mk)
+
+# clean-up all unknown PRODUCT_PACKAGES
+allowed_list := product_manifest.xml
+allowed_list += android.hardware.health@2.0-impl-default.recovery
+allowed_list += DeviceDiagnostics
+$(call enforce-product-packages-exist, $(allowed_list))

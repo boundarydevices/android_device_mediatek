b214ab98ebdfa8761a4e1765bf5714fbde0b5a24
Esteban Blanc <eblanc@baylibre.com>
2024-11-19 15:03:02 +0000

    i700_evk: use common mt8188 platform
    The mt8390 platform has been renamed to mt8188 as it will be used by
    both mt8390 and mt8370, both based on mt8188 SoC
    
    Signed-off-by: Esteban Blanc <eblanc@baylibre.com>

--- i700_evk/BoardConfig.mk
+++ i700_evk/BoardConfig.mk
@@ -14,7 +14,7 @@
 # limitations under the License.
 #
 
-include device/mediatek/common/mt8390/BoardConfig.mk
+include device/mediatek/common/mt8188/BoardConfig.mk
 
 TARGET_BOOTLOADER_BOARD_NAME := i700_evk
 
--- i700_evk/i700_evk.mk
+++ i700_evk/i700_evk.mk
@@ -22,7 +22,7 @@ TEE_KEYMASTER_GATEKEEPER_ENABLE := true
 
 BOOTLOADERS_BINARIES := device/mediatek/boards-binaries/i700_evk
 
-$(call inherit-product, device/mediatek/common/mt8390/mt8390.mk)
+$(call inherit-product, device/mediatek/common/mt8188/mt8390.mk)
 
 PRODUCT_NAME := i700_evk
 PRODUCT_DEVICE := i700_evk

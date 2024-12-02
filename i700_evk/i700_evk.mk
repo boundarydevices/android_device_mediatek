be464c8c1c0c756a5bdce3935356dee96aeb7eef
Julien Masson <jmasson@baylibre.com>
2024-12-02 11:19:22 +0100

    treewide: support only 64-bit architecture
    There is no reason to keep support of 32 bits.
    All of our prebuilts binaries/libraries are not always built for 32
    built and by enabling only 64 bits apps/services performance are
    better.
    
    Signed-off-by: Julien Masson <jmasson@baylibre.com>

--- i700_evk/i700_evk.mk
+++ i700_evk/i700_evk.mk
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 # All OP-TEE vars MUST be set before including common/mt*

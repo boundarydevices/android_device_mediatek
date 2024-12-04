46b9f5f1132c536797f6af03fd4f68659ce2795f
Julien Masson <jmasson@baylibre.com>
2024-12-04 11:54:38 +0100

    device: upgrade PRODUCT_SHIPPING_API_LEVEL to 34
    Shipping API level has been upgraded to Android 14 (34).
    
    Signed-off-by: Julien Masson <jmasson@baylibre.com>

--- device.mk
+++ device.mk
@@ -63,8 +63,8 @@ $(call inherit-product, device/mediatek/binaries/device.mk)
 #  not related to any feature folder are placed here.                 #
 #######################################################################
 
-# Shipping API level to Android 13 (T)
-PRODUCT_SHIPPING_API_LEVEL := 33
+# Shipping API level to Android 14
+PRODUCT_SHIPPING_API_LEVEL := 34
 
 # Set Vendor SPL to match platform
 VENDOR_SECURITY_PATCH = $(PLATFORM_SECURITY_PATCH)

734cb664c9e6f38a159a7f5af467172bc725738f
Julien Masson <jmasson@baylibre.com>
2024-12-09 11:14:53 +0100

    kernel: no GKI support
    We don't support yet kernel GKI, all unecessary code has been removed
    and BOARD_USES_GENERIC_KERNEL_IMAGE set to false.
    
    Signed-off-by: Julien Masson <jmasson@baylibre.com>

--- kernel/BoardConfig.mk
+++ kernel/BoardConfig.mk
@@ -17,6 +17,9 @@
 # sepolicy
 BOARD_SEPOLICY_DIRS += device/mediatek/kernel/sepolicy
 
+# No GKI support
+BOARD_USES_GENERIC_KERNEL_IMAGE := false
+
 # bootimage generation
 BOARD_KERNEL_BASE = 0x40000000
 BOARD_KERNEL_OFFSET = 0x00200000
@@ -64,52 +67,3 @@ endif
 BOARD_RECOVERY_KERNEL_MODULES := \
     vendor/mediatek/prebuilts/kernel/$(TARGET_KERNEL_USE)/mediatek-drm.ko \
     vendor/mediatek/prebuilts/kernel/$(TARGET_KERNEL_USE)/mediatek-drm-hdmi.ko
-
-ifneq ($(MTK_USES_GKI),)
-
-# Built as module when using GKI
-BOARD_VENDOR_KERNEL_MODULES += \
-vendor/mediatek/prebuilts/kernel/$(TARGET_KERNEL_USE)/goodix.ko \
-vendor/mediatek/prebuilts/kernel/$(TARGET_KERNEL_USE)/phy-mtk-hdmi-drv.ko \
-vendor/mediatek/prebuilts/kernel/$(TARGET_KERNEL_USE)/phy-mtk-tphy.ko \
-vendor/mediatek/prebuilts/kernel/$(TARGET_KERNEL_USE)/pwm-mtk-disp.ko \
-vendor/mediatek/prebuilts/kernel/$(TARGET_KERNEL_USE)/snd-soc-mt6358.ko \
-vendor/mediatek/prebuilts/kernel/$(TARGET_KERNEL_USE)/mtk-kpd.ko \
-vendor/mediatek/prebuilts/kernel/$(TARGET_KERNEL_USE)/tcpci_mt6360.ko \
-vendor/mediatek/prebuilts/kernel/$(TARGET_KERNEL_USE)/v4l2-fwnode.ko \
-vendor/mediatek/prebuilts/kernel/$(TARGET_KERNEL_USE)/mtk-seninf.ko \
-vendor/mediatek/prebuilts/kernel/$(TARGET_KERNEL_USE)/phy-generic.ko \
-vendor/mediatek/prebuilts/kernel/$(TARGET_KERNEL_USE)/tusb322i.ko \
-vendor/mediatek/prebuilts/kernel/$(TARGET_KERNEL_USE)/mtu3.ko \
-vendor/mediatek/prebuilts/kernel/$(TARGET_KERNEL_USE)/xhci-mtk-hcd.ko \
-vendor/mediatek/prebuilts/kernel/$(TARGET_KERNEL_USE)/musb_hdrc.ko \
-vendor/mediatek/prebuilts/kernel/$(TARGET_KERNEL_USE)/mediatek.ko \
-
-# Necessary modules to boot the board to the userland
-BOARD_VENDOR_RAMDISK_KERNEL_MODULES += \
-vendor/mediatek/prebuilts/kernel/$(TARGET_KERNEL_USE)/mt6397.ko \
-vendor/mediatek/prebuilts/kernel/$(TARGET_KERNEL_USE)/btmtkuart.ko \
-vendor/mediatek/prebuilts/kernel/$(TARGET_KERNEL_USE)/mediatek-cpufreq.ko \
-vendor/mediatek/prebuilts/kernel/$(TARGET_KERNEL_USE)/hwmon.ko \
-vendor/mediatek/prebuilts/kernel/$(TARGET_KERNEL_USE)/ntc_thermistor.ko \
-vendor/mediatek/prebuilts/kernel/$(TARGET_KERNEL_USE)/kfifo_buf.ko \
-vendor/mediatek/prebuilts/kernel/$(TARGET_KERNEL_USE)/nvmem_mtk-efuse.ko \
-vendor/mediatek/prebuilts/kernel/$(TARGET_KERNEL_USE)/regmap-spmi.ko \
-vendor/mediatek/prebuilts/kernel/$(TARGET_KERNEL_USE)/mt6311-regulator.ko \
-vendor/mediatek/prebuilts/kernel/$(TARGET_KERNEL_USE)/mt6315-regulator.ko \
-vendor/mediatek/prebuilts/kernel/$(TARGET_KERNEL_USE)/mt6323-regulator.ko \
-vendor/mediatek/prebuilts/kernel/$(TARGET_KERNEL_USE)/mt6358-regulator.ko \
-vendor/mediatek/prebuilts/kernel/$(TARGET_KERNEL_USE)/mt6380-regulator.ko \
-vendor/mediatek/prebuilts/kernel/$(TARGET_KERNEL_USE)/mt6397-regulator.ko \
-vendor/mediatek/prebuilts/kernel/$(TARGET_KERNEL_USE)/mtk_wdt.ko \
-vendor/mediatek/prebuilts/kernel/$(TARGET_KERNEL_USE)/cqhci.ko \
-vendor/mediatek/prebuilts/kernel/$(TARGET_KERNEL_USE)/mtk-sd.ko \
-vendor/mediatek/prebuilts/kernel/$(TARGET_KERNEL_USE)/mtk-svs.ko \
-vendor/mediatek/prebuilts/kernel/$(TARGET_KERNEL_USE)/matrix-keymap.ko \
-vendor/mediatek/prebuilts/kernel/$(TARGET_KERNEL_USE)/mtk-pmic-keys.ko \
-vendor/mediatek/prebuilts/kernel/$(TARGET_KERNEL_USE)/mtk-pmic-wrap.ko \
-# Uart still built-in for debug purposes, but long term it shall be built as modules
-#vendor/mediatek/prebuilts/kernel/$(TARGET_KERNEL_USE)/8250_mtk.ko \
-#vendor/mediatek/prebuilts/kernely/$(TARGET_KERNEL_USE)/mtk-uart-apdma.ko \
-
-endif

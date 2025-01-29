a52c10273e0aa764aa2a2ebd947064b1776f0419
Vitor Sato Eschholz <vsatoes@baylibre.com>
2025-01-29 14:05:07 +0000

    i700_evk: add dtb overlay to enable dsi display
    Add device tree overlay to enable DSI display with panel startek
    kd070fhfid078.
    
    Signed-off-by: Vitor Sato Eschholz <vsatoes@baylibre.com>

--- boards/i700_evk/BoardConfig.mk
+++ boards/i700_evk/BoardConfig.mk
@@ -41,4 +41,5 @@ DTB_FILES := \
 
 # Please keep this list fixed: add new files in the end of the list
 DTBO_FILES := \
-    $(MTK_KERNEL_DIST)/mt8390-genio-700-evk-android.dtb
+    $(MTK_KERNEL_DIST)/mt8390-genio-700-evk-android.dtb \
+    $(MTK_KERNEL_DIST)/mt8390-genio-700-evk-android-dsi.dtb

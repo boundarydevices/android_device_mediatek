ebf6e1c33a696248ce47c089c6dd8238d6ff168e
Vitor Sato Eschholz <vsatoes@baylibre.com>
2025-03-13 09:38:32 +0000

    wifi: add wifi support for mt7663
    Add WiFi support for mt7663 chip.
    
    Boards with mt7663 chip should include mt7663/BoardConfig.mk and
    mt7663/inherit mt7663.mk to enable it.
    
    Note: added support based on the open source option given by [1].
    
    [1]: https://gitlab.baylibre.com/baylibre/mediatek/rita/vendor/mediatek/wireless/
         HEAD at 3057fa758247370f0f3237a32616de98d22912a7
    
    Signed-off-by: Vitor Sato Eschholz <vsatoes@baylibre.com>

---
+++ wifi/mt7663/BoardConfig.mk
@@ -0,0 +1,25 @@
+#
+# Copyright 2025 BayLibre SAS
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+include device/mediatek/wifi/BoardConfig.mk
+
+# Kernel modules
+BOARD_VENDOR_KERNEL_MODULES += \
+    vendor/mediatek/prebuilts/kernel/$(TARGET_KERNEL_USE)/mt76.ko \
+    vendor/mediatek/prebuilts/kernel/$(TARGET_KERNEL_USE)/mt76-sdio.ko \
+    vendor/mediatek/prebuilts/kernel/$(TARGET_KERNEL_USE)/mt7615-common.ko \
+    vendor/mediatek/prebuilts/kernel/$(TARGET_KERNEL_USE)/mt7663-usb-sdio-common.ko \
+    vendor/mediatek/prebuilts/kernel/$(TARGET_KERNEL_USE)/mt7663s.ko

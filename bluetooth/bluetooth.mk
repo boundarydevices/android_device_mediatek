bd35f547cec45361d96b29f5a46d37d600c847a5
Vitor Sato Eschholz <vsatoes@baylibre.com>
2025-03-14 09:59:24 +0000

    bluetooth: add bluetooh support for mt7663
    Add Bluetooth support for mt7663 chip.
    
    Boards with mt7663 chip should include mt7663/BoardConfig.mk and
    mt7663/inherit mt7663.mk to enable it.
    
    Note: added support based on [1].
    
    [1]: https://gitlab.baylibre.com/baylibre/mediatek/rita/vendor/mediatek/wireless/
         HEAD at 3057fa758247370f0f3237a32616de98d22912a7
    
    Signed-off-by: Vitor Sato Eschholz <vsatoes@baylibre.com>

---
+++ bluetooth/bluetooth.mk
@@ -0,0 +1,47 @@
+#
+# Copyright 2025 BayLibre SAS
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Framework config files
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
+    frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml
+
+# Bluetooth HAL service
+PRODUCT_PACKAGES += \
+    android.hardware.bluetooth@1.1-service.btlinux
+
+# Enable all supported Bluetooth profiles
+PRODUCT_PROPERTY_OVERRIDES += \
+    bluetooth.profile.a2dp.source.enabled=true \
+    bluetooth.profile.asha.central.enabled=true \
+    bluetooth.profile.avrcp.target.enabled=true \
+    bluetooth.profile.bap.broadcast.assist.enabled=true \
+    bluetooth.profile.bap.unicast.client.enabled=true \
+    bluetooth.profile.bas.client.enabled=true \
+    bluetooth.profile.ccp.server.enabled=true \
+    bluetooth.profile.csip.set_coordinator.enabled=true \
+    bluetooth.profile.gatt.enabled=true \
+    bluetooth.profile.hap.client.enabled=true \
+    bluetooth.profile.hfp.ag.enabled=true \
+    bluetooth.profile.hid.device.enabled=true \
+    bluetooth.profile.hid.host.enabled=true \
+    bluetooth.profile.map.server.enabled=true \
+    bluetooth.profile.mcp.server.enabled=true \
+    bluetooth.profile.opp.enabled=true \
+    bluetooth.profile.pan.nap.enabled=true \
+    bluetooth.profile.pan.panu.enabled=true \
+    bluetooth.profile.pbap.server.enabled=true \
+    bluetooth.profile.vcp.controller.enabled=true

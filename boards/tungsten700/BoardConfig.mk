2fab758c8d339be9ab8e14b0633b4563e1b8ed1c
Gary Bisson <gary.bisson@ezurio.com>
2025-04-01 14:28:39 +0200

    boards: tungsten700: initial addition
    - Mostly the same as the evk with a different eMMC size (16GB)
    - Enable AVB config by default
    - Set serial baudrate to 115200
    
    Signed-off-by: Gary Bisson <gary.bisson@ezurio.com>

---
+++ boards/tungsten700/BoardConfig.mk
@@ -0,0 +1,56 @@
+#
+# Copyright 2014 The Android Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# enable AVB by default
+TARGET_AVB_ENABLE:=true
+
+# override default serial speed
+BOARD_KERNEL_SERIAL_SPEED := 115200
+
+include device/mediatek/soc/mt8188/BoardConfig.mk
+
+TARGET_BOOTLOADER_BOARD_NAME := tungsten700
+
+# kernel commandline
+BOARD_KERNEL_CMDLINE += \
+    androidboot.boot_devices=soc/11230000.mmc \
+    androidboot.hardware=mt8390 quiet \
+    androidboot.serialno=tungsten700
+
+# FS configuration
+BOARD_SUPER_PARTITION_SIZE            := 4831838208
+BOARD_DTBOIMG_PARTITION_SIZE          := 4194304
+BOARD_BOOTIMAGE_PARTITION_SIZE        := 50331648
+BOARD_INIT_BOOT_IMAGE_PARTITION_SIZE  := 16777216
+BOARD_VENDOR_BOOTIMAGE_PARTITION_SIZE := 33554432
+BOARD_DB_DYNAMIC_PARTITIONS_SIZE      := 2411724800
+
+MTK_PARTITIONS_YAML := device/mediatek/boards/tungsten700/partitions.yaml
+
+# Please keep this list fixed: add new files in the end of the list
+DTB_FILES := \
+    $(MTK_KERNEL_DIST)/mt8390-tungsten-smarc.dtb
+
+# Please keep this list fixed: add new files in the end of the list
+DTBO_FILES := \
+    $(MTK_KERNEL_DIST)/mt83x0-tungsten-smarc-android-dsi-tm070jdhg30.dtb \
+    $(MTK_KERNEL_DIST)/mt83x0-tungsten-smarc-android.dtb
+
+# Bluetooth
+include device/mediatek/bluetooth/BoardConfig.mk
+
+# WiFi
+include device/mediatek/wifi/BoardConfig.mk

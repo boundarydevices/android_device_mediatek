975c0cb679f97eeed2258521c172cbf4e064c3b6
Vitor Sato Eschholz <vsatoes@baylibre.com>
2025-04-02 15:12:22 +0000

    audio: Fix audio policy for HAL V7
    We should use the correct version and policies since we are using
    HAL v7. To do so, run:
    
    $ hardware/interfaces/audio/7.0/config/update_audio_policy_config.sh
      device/mediatek/boards/i350_evk/xml/audio_policy_configuration-i350_evk.xml 6.0
    
    Then, manually edit the included file.
    
    Signed-off-by: Vitor Sato Eschholz <vsatoes@baylibre.com>

--- audio/device.mk
+++ audio/device.mk
@@ -30,7 +30,7 @@ PRODUCT_PACKAGES += \
 # Audio policy configuration
 USE_XML_AUDIO_POLICY_CONF := 1
 PRODUCT_COPY_FILES += \
-    frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_audio_policy_configuration.xml \
+    frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration_7_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_audio_policy_configuration_7_0.xml \
     frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \

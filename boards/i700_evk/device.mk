4103294a47d33d99535bd1402804428c04f3c781
Vitor Sato Eschholz <vsatoes@baylibre.com>
2025-04-02 15:05:49 +0000

    boards: i700_evk: add audio support for genio-700-evk
    Add basic audio support for genio-700-evk:
    - Output on Jack Earphone
    - Input on DMIC
    
    Signed-off-by: Vitor Sato Eschholz <vsatoes@baylibre.com>

--- boards/i700_evk/device.mk
+++ boards/i700_evk/device.mk
@@ -24,6 +24,13 @@ $(call inherit-product, device/mediatek/bluetooth/mt7921/mt7921.mk)
 
 # WiFi
 $(call inherit-product, device/mediatek/wifi/mt7921/mt7921.mk)
+
+# Audio
+$(call inherit-product, device/mediatek/audio/device.mk)
+PRODUCT_PACKAGES += audio.primary.i700_evk
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/xml/audio_policy_configuration-i700_evk.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
+    $(LOCAL_PATH)/xml/audio_hal_configuration-i700_evk.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio.i700_evk.xml
 endif
 
 # ueventd

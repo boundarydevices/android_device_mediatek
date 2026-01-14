0d5809affe3a22f4aa78ca02f2984487eeb2023a
Don Ferencz <Donald.Ferencz@ezurio.com>
2026-01-14 14:02:06 -0500

    boards: mtk: Add Tungsten 510 HMI
    Signed-off-by: Don Ferencz <Donald.Ferencz@ezurio.com>

---
+++ boards/tungsten510hmi/device.mk
@@ -0,0 +1,37 @@
+#
+# Copyright 2014 The Android Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# common mediatek board
+$(call inherit-product, device/mediatek/boards/board.mk)
+
+# Bluetooth
+$(call inherit-product, device/mediatek/bluetooth/mt7921/mt7921.mk)
+
+# WiFi
+$(call inherit-product, device/mediatek/wifi/mt7921/mt7921.mk)
+
+# ueventd
+PRODUCT_COPY_FILES += \
+    device/mediatek/boards/tungsten510hmi/ueventd.rc:$(TARGET_COPY_OUT_VENDOR)/etc/ueventd.rc
+
+# Audio
+$(call inherit-product, device/mediatek/audio/device.mk)
+PRODUCT_PACKAGES += audio.primary.tungsten510hmi
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/xml/audio_policy_configuration-tungsten510hmi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
+    $(LOCAL_PATH)/xml/audio_hal_configuration-tungsten510hmi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio.tungsten510hmi.xml
+
